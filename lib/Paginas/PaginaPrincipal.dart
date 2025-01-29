2704d1d425931d85ddfe30c84778d733cd163b1e
Laia Barba Tey <laiabarba@ceroca.cat>
2025-01-29 17:38:15 +0100

    Container producto
    crear el container para ver los productos

--- lib/Paginas/PaginaPrincipal.dart
+++ lib/Paginas/PaginaPrincipal.dart
@@ -1,4 +1,5 @@
 import 'package:applicacion_libre_davih_laia/Componentes/AppbarPer.dart';
+import 'package:applicacion_libre_davih_laia/Componentes/ContainerProducto.dart';
 import 'package:applicacion_libre_davih_laia/Componentes/DrawerPer.dart';
 import 'package:applicacion_libre_davih_laia/Data/BBDD.dart';
 import 'package:flutter/material.dart';
@@ -16,7 +17,7 @@ class _PaginaprincipalState extends State<Paginaprincipal> {
   // BaseDeDades db = BaseDeDades();
 
   List productos = [
-    {"Nombre": "Bici", "Precio": "50", "Imagen": "bici.png"}
+    {"Nombre": "Bici", "Precio": "50", "Imagen": "bici.jpg"}
   ];
 
   // void anadirCarrito(String nombre, int precio) {
@@ -37,25 +38,12 @@ class _PaginaprincipalState extends State<Paginaprincipal> {
           body: ListView.builder(
             itemCount: productos.length,
             itemBuilder: (context, index) {
-              return Container(
-                child: Column(
-                  children: [
-                    Text(productos[index]["Nombre"]),
-                    Text(productos[index]["Precio"]),
-                    Text(productos[index]["Imagen"]),
 
-                  ],
-                ),
-              );
-              
-              
-              // ItemTasca(
-              //   TextTasca: db.tasquesLlistes[index]["Titol"].toString(),
-              //   valorCheckbox: db.tasquesLlistes[index]["Valor"],
-              //   canviarValorCheckbox: (valor) =>
-              //       canviarCheckbox(db.tasquesLlistes[index]["Valor"], index),
-              //   esborrarTasca: (valor) => accioEsborrarTasca(index),
-              // );
+              return Containerproducto(
+                Nombre: productos[index]["Nombre"], 
+                Precio: productos[index]["Precio"], 
+                Imagen: productos[index]["Imagen"]);
+            
             },
           ),
         ),
