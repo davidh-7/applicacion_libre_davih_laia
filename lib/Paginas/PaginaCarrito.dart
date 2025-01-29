81ff1b9c3c696268792ab737e7f242ec7d9e5c17
Laia Barba Tey <laiabarba@ceroca.cat>
2025-01-29 17:23:52 +0100

    BBDD
    Crear BBDD  + lista de productos

--- lib/Paginas/PaginaCarrito.dart
+++ lib/Paginas/PaginaCarrito.dart
@@ -1,9 +1,52 @@
 import 'package:applicacion_libre_davih_laia/Componentes/AppbarPer.dart';
 import 'package:applicacion_libre_davih_laia/Componentes/DrawerPer.dart';
+import 'package:applicacion_libre_davih_laia/Data/BBDD.dart';
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
 
-class Paginacarrito extends StatelessWidget {
+class Paginacarrito extends StatefulWidget {
   const Paginacarrito({super.key});
+
+  @override
+  State<Paginacarrito> createState() => _PaginacarritoState();
+}
+
+class _PaginacarritoState extends State<Paginacarrito> {
+
+  final Box _boxHive = Hive.box("box_carrito_app");
+  BaseDeDades db = BaseDeDades();
+
+
+  @override
+  void initState(){
+    if(_boxHive.get("box_tasques_app") == null){
+      db.generarDadesEx();
+    }else{
+      db.carregarDades();
+    }
+    super.initState();
+  }
+
+  // void accioGuardar(){
+  //   setState(() {
+  //     db.productosCarrito.add({"Titol": tecTextTasca.text, "Valor": false});
+  //   });
+  //   db.actualitzarDades();
+  // }
+
+
+  void canviarCheckbox(bool? valorCheckbox, int index){
+    setState(() {
+      db.productosCarrito[index]["Valor"] = !db.productosCarrito[index]["Valor"];
+    });
+    db.actualitzarDades();
+  }
+  void accioEsborrarTasca(int index) {
+    setState(() {
+      db.productosCarrito.removeAt(index);
+    });
+    db.actualitzarDades();
+  }
 
   @override
   Widget build(BuildContext context) {
--- lib/Paginas/PaginaPrincipal.dart
+++ lib/Paginas/PaginaPrincipal.dart
@@ -1,20 +1,63 @@
 import 'package:applicacion_libre_davih_laia/Componentes/AppbarPer.dart';
 import 'package:applicacion_libre_davih_laia/Componentes/DrawerPer.dart';
+import 'package:applicacion_libre_davih_laia/Data/BBDD.dart';
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
 
-class Paginaprincipal extends StatelessWidget {
+class Paginaprincipal extends StatefulWidget {
   const Paginaprincipal({super.key});
+
+  @override
+  State<Paginaprincipal> createState() => _PaginaprincipalState();
+}
+
+class _PaginaprincipalState extends State<Paginaprincipal> {
+  // final Box _boxHive = Hive.box("box_carrito_app");
+  // BaseDeDades db = BaseDeDades();
+
+  List productos = [
+    {"Nombre": "Bici", "Precio": "50", "Imagen": "bici.png"}
+  ];
+
+  // void anadirCarrito(String nombre, int precio) {
+  //   setState(() {
+  //     db.productosCarrito.add({"Nombre": nombre, "Precio": precio});
+  //   });
+  //   db.actualitzarDades();
+  // }
 
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Container(
         color: Colors.pink,
-
         child: Scaffold(
-          
           drawer: Drawerper(),
           appBar: appbarPer(),
+          body: ListView.builder(
+            itemCount: productos.length,
+            itemBuilder: (context, index) {
+              return Container(
+                child: Column(
+                  children: [
+                    Text(productos[index]["Nombre"]),
+                    Text(productos[index]["Precio"]),
+                    Text(productos[index]["Imagen"]),
+
+                  ],
+                ),
+              );
+              
+              
+              // ItemTasca(
+              //   TextTasca: db.tasquesLlistes[index]["Titol"].toString(),
+              //   valorCheckbox: db.tasquesLlistes[index]["Valor"],
+              //   canviarValorCheckbox: (valor) =>
+              //       canviarCheckbox(db.tasquesLlistes[index]["Valor"], index),
+              //   esborrarTasca: (valor) => accioEsborrarTasca(index),
+              // );
+            },
+          ),
         ),
       ),
     );
