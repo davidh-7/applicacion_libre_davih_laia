29476a1a00b29518a609a9f5dd9a260f657b739a
Laia Barba Tey <laiabarba@ceroca.cat>
2025-01-29 16:45:41 +0100

    PaginaCarrito
    ahora se a hecho el diseño,

--- lib/Paginas/PaginaCarrito.dart
+++ lib/Paginas/PaginaCarrito.dart
@@ -1,3 +1,5 @@
+import 'package:applicacion_libre_davih_laia/Componentes/AppbarPer.dart';
+import 'package:applicacion_libre_davih_laia/Componentes/DrawerPer.dart';
 import 'package:flutter/material.dart';
 
 class Paginacarrito extends StatelessWidget {
@@ -7,9 +9,94 @@ class Paginacarrito extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
       child: Container(
+        
         child: Scaffold(
+          backgroundColor: Colors.blueGrey[500],
+          drawer: Drawerper(),
+          appBar: appbarPer(),
           body: Center(
-            child: Text("Hola"),
+            child: Padding(
+              padding: const EdgeInsets.all(30.0),
+              child: Container(
+                width: double.infinity,
+                
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.blueGrey[200],
+                ),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Center(
+                        child: Text(
+                          "CARRITO DE LA COMPRA",
+                          style: TextStyle(
+                            fontSize: 30
+                          ),
+                        )
+                      ),
+                    ),
+                    SizedBox(height: 15,),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Padding(
+                            padding: const EdgeInsets.all(10.0),
+                            child: Column(
+                              children: [
+                                Container(
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(20),
+                                    color: Colors.blueGrey[500],
+                                  ),
+                                  width: double.infinity,
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Column(
+                                        children: [
+                                          Text("PRODUCTOS")
+                                        ],
+                                      ),
+                                    ),
+                                  
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          child: Padding(
+                            padding: const EdgeInsets.all(10.0),
+                            child: Column(
+                              children: [
+                                Container(
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(20),
+                                    
+                                    color: Colors.blueGrey[500],
+                                  ),
+                                  width: double.infinity,
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Column(
+                                        children: [
+                                          Text("TOTAL: ")
+                                        ],
+                                      ),
+                                    ),
+                                  
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            )
           ),
         ),
       ),
